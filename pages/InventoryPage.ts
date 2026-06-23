5796ca3d718e6eb401d7259fed0553f874d9bb85
Swap <77547008+Swapna172S@users.noreply.github.com>
2026-06-23 16:37:46 +0530

    Create InventoryPage class for managing inventory actions

---
+++ pages/InventoryPage.ts
@@ -0,0 +1,14 @@
+
+export class InventoryPage {
+  constructor(private page) {}
+
+  addToCartButton = (item: string) =>
+    this.page.locator(`text=${item}`).locator('..').getByRole('button');
+
+  cartIcon = this.page.locator('.shopping_cart_link');
+  cartBadge = this.page.locator('.shopping_cart_badge');
+
+  async addItem(item: string) {
+    await this.addToCartButton(item).click();
+  }
+}
