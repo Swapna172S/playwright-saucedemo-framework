bee1a2020430c6ab601d76a0d504bcd375db0dcd
Swap <77547008+Swapna172S@users.noreply.github.com>
2026-06-23 16:38:50 +0530

    Implement CheckoutPage class for order processing

---
+++ pages/CheckoutPage.ts
@@ -0,0 +1,15 @@
+
+export class CheckoutPage {
+  constructor(private page) {}
+
+  async fillDetails() {
+    await this.page.fill('#first-name', 'Test');
+    await this.page.fill('#last-name', 'User');
+    await this.page.fill('#postal-code', '400001');
+  }
+
+  async completeOrder() {
+    await this.page.click('#continue');
+    await this.page.click('#finish');
+  }
+}
