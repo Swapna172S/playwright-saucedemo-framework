ab0bedcfb7079d34b6016b8e758709fe11235a93
Swap <77547008+Swapna172S@users.noreply.github.com>
2026-06-27 22:36:02 +0530

    Refactor InventoryPage methods and properties
    Refactor addToCartButton to filter by item text and convert cartIcon and cartBadge to getters.

--- pages/InventoryPage.ts
+++ pages/InventoryPage.ts
@@ -1,14 +1,30 @@
+import { Page } from '@playwright/test';
 
 export class InventoryPage {
   constructor(private page: Page) {}
 
-  addToCartButton = (item: string) =>
-    this.page.locator(`text=${item}`).locator('..').getByRole('button');
+   addToCartButton = (item: string) =>
+    this.page.locator('.inventory_item').filter({
+      hasText: item
+    }).getByRole('button', { name: 'Add to cart' });
 
-  cartIcon = this.page.locator('.shopping_cart_link');
-  cartBadge = this.page.locator('.shopping_cart_badge');
+
+    
+  // FIX: use getter
+  get cartIcon() {
+    return this.page.locator('.shopping_cart_link');
+  }
+
+
+  get cartBadge() {
+    return this.page.locator('.shopping_cart_badge');
+  }
 
   async addItem(item: string) {
+    // ensure page loaded
+    await this.page.waitForLoadState('domcontentloaded');
+
     await this.addToCartButton(item).click();
   }
+  
 }
